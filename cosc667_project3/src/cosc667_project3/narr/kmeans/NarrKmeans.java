08adf755f2b2a0977687c1516624e97f18a48752
Nicole Arruda <narruda@emich.edu>
2016-04-03 14:09:54 -0400

    Finished copying Kmeans code.

--- cosc667_project3/src/cosc667_project3/narr/kmeans/NarrKmeans.java
+++ cosc667_project3/src/cosc667_project3/narr/kmeans/NarrKmeans.java
@@ -1,7 +1,9 @@
 package cosc667_project3.narr.kmeans;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
@@ -56,6 +58,151 @@ public class NarrKmeans {
 
 			inFile.close();
 		}
+	}
+
+	public void setParameters(int numberClusters, int seed) {
+		this.numberClusters = numberClusters;
+
+		this.random = new Random(seed);
+	}
+
+	public void cluster() {
+		this.initializeClusters();
+
+		this.initializeCentroids();
+
+		boolean stopCondition = false;
+
+		while (!stopCondition) {
+			int clusterChanges = this.assignClusters();
+
+			this.updateCentroids();
+
+			stopCondition = clusterChanges == 0;
+		}
+	}
+
+	private void updateCentroids() {
+		ArrayList<NarrRecord> clusterSum = new ArrayList<NarrRecord>();
+
+		for (int i = 0; i < this.numberClusters; i++) {
+			double[] attributes = new double[this.numberAttributes];
+			for (int j = 0; j < this.numberAttributes; j++) {
+				attributes[j] = 0;
+			}
+
+			clusterSum.add(new NarrRecord(attributes));
+		}
+
+		int[] clusterSizes = new int[this.numberClusters];
+
+		for (int i = 0; i < this.numberRecords; i++) {
+			clusterSizes[i] = 0;
+		}
+
+		for (int i = 0; i < this.numberRecords; i++) {
+			int cluster = this.clusters[i];
+
+			NarrRecord sum = this.sum(clusterSum.get(cluster), this.records.get(i));
+			clusterSum.set(cluster, sum);
+
+			clusterSizes[cluster] += 1;
+		}
+
+		for (int i = 0; i < this.numberClusters; i++) {
+			NarrRecord average = this.scale(clusterSum.get(i), 1.0 / clusterSizes[i]);
+
+			this.centroids.set(i, average);
+		}
+	}
+
+	private NarrRecord scale(NarrRecord record, double scalar) {
+		double[] scaledAttributes = new double[record.attributes.length];
+
+		for (int i = 0; i < record.attributes.length; i++) {
+			scaledAttributes[i] = scalar * record.attributes[i];
+		}
+
+		return new NarrRecord(scaledAttributes);
+	}
+
+	private NarrRecord sum(NarrRecord firstRecord, NarrRecord secondRecord) {
+		double[] sumsOfAttributes = new double[firstRecord.attributes.length];
+
+		for (int i = 0; i < firstRecord.attributes.length; i++) {
+			sumsOfAttributes[i] = firstRecord.attributes[i] + secondRecord.attributes[i];
+		}
+
+		return new NarrRecord(sumsOfAttributes);
+	}
+
+	private int assignClusters() {
+		int clusterChanges = 0;
+
+		for (int i = 0; i < this.numberRecords; i++) {
+			NarrRecord record = this.records.get(i);
+
+			double minDistance = this.distance(record, this.centroids.get(0));
+			int minIndex = 0;
+
+			for (int j = 0; j < this.numberClusters; j++) {
+				double distance = this.distance(record, this.centroids.get(j));
+
+				if (distance < minDistance) {
+					minDistance = distance;
+					minIndex = j;
+				}
+			}
+
+			if (this.clusters[i] != minIndex) {
+				this.clusters[i] = minIndex;
+				clusterChanges++;
+			}
+		}
+
+		return clusterChanges;
+	}
+
+	private double distance(NarrRecord firstRecord, NarrRecord secondRecord) {
+		double sum = 0;
+
+		for (int i = 0; i < firstRecord.attributes.length; i++) {
+			sum += Math.pow((firstRecord.attributes[i] - secondRecord.attributes[i]), 2);
+		}
+
+		return sum;
+	}
+
+	private void initializeCentroids() {
+		this.centroids = new ArrayList<NarrRecord>();
+
+		for (int i = 0; i < this.numberClusters; i++) {
+			int index = this.random.nextInt(this.numberRecords);
+
+			this.centroids.add(this.records.get(index));
+		}
+	}
+
+	private void initializeClusters() {
+		this.clusters = new int[this.numberRecords];
+
+		for (int i = 0; i < this.numberRecords; i++) {
+			this.clusters[i] = -1;
+		}
+	}
+
+	public void display(String outputFile) throws IOException {
+		PrintWriter outFile = new PrintWriter(new FileWriter(outputFile));
+
+		for (int i = 0; i < this.numberRecords; i++) {
+			for (int j = 0; j < this.numberAttributes; j++) {
+				outFile.print(this.records.get(i).attributes[j] + " ");
+			}
+
+			outFile.println(this.clusters[i] + 1);
+		}
+
+		outFile.close();
 	}
 
 }
--- cosc667_project3/src/cosc667_project3/narr/kmeans/NarrKmeansDriver.java
+++ cosc667_project3/src/cosc667_project3/narr/kmeans/NarrKmeansDriver.java
@@ -1,10 +1,22 @@
 package cosc667_project3.narr.kmeans;
 
+import java.io.IOException;
+
 public class NarrKmeansDriver {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		String inputFile = "inputFile";
+		String outputFile = "outputFile";
 
-		System.out.println("Sup ma Plutes");
+		NarrKmeans clustering = new NarrKmeans();
+
+		clustering.load(inputFile);
+
+		clustering.setParameters(2, 4539);
+
+		clustering.cluster();
+
+		clustering.display(outputFile);
 	}
 
 }
