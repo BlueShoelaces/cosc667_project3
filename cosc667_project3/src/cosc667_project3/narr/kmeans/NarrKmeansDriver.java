31b651223cc5b8c9411d3f9e1d094df8aa0b60a2
Nicole Arruda <narruda789@gmail.com>
2016-04-03 16:09:38 -0400

    Adding file system. Driver should work...

--- cosc667_project3/src/cosc667_project3/narr/kmeans/NarrKmeansDriver.java
+++ cosc667_project3/src/cosc667_project3/narr/kmeans/NarrKmeansDriver.java
@@ -1,12 +1,19 @@
 package cosc667_project3.narr.kmeans;
 
+import java.io.File;
 import java.io.IOException;
 
 public class NarrKmeansDriver {
 
 	public static void main(String[] args) throws IOException {
-		String inputFile = "inputFile";
-		String outputFile = "outputFile";
+		
+		// Set delimiter to / or \ according to operating system
+		final String DELIMITER = File.separator;
+				
+		String root = DELIMITER + "Resources";
+		
+		String inputFile = root + DELIMITER + "Part1" + DELIMITER + "Input" + DELIMITER + "file1";
+		String outputFile = root + DELIMITER + "Part1" + DELIMITER + "Output" + DELIMITER + "output1.txt";
 
 		NarrKmeans clustering = new NarrKmeans();
 
